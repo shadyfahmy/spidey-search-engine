330c48707c05c5ab02f778d64ac32b4bd964740b
Shady Fahmy <shadyfahmy67@gmail.com>
2020-04-25 11:16:11 +0200

    New spring boot project

---
+++ queryprocessor/src/main/java/com/shady/queryprocessor/QueryProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.shady.queryprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryProcessorApplication.class, args);
+	}
+
+}
