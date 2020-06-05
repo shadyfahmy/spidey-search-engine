5a5a44f041d2a62a6c176af075990368794a0042
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-06-05 16:22:42 +0200

    remove not used imports in query processor

--- query_processor/src/main/java/com/search/queryprocessor/QueryprocessorApplication.java
+++ query_processor/src/main/java/com/search/queryprocessor/QueryprocessorApplication.java
@@ -3,20 +3,12 @@ package com.search.queryprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class QueryprocessorApplication {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(QueryprocessorApplication.class, args);
-
 	}
 
 }
